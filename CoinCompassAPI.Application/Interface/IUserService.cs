e629b0bdcc0b8e792a3cacf3988a31c189a0bdd7
Rafael de Oliveira Gomes <rafinha.gomo.11.og@gmail.com>
2024-12-20 16:52:49 -0300

    configuração do JWT e lógica para usuario

---
+++ CoinCompassAPI.Application/Interface/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using CoinCompassAPI.Application.DTOs.Account;
+using CoinCompassAPI.Application.DTOs.User;
+using CoinCompassAPI.Domain.Entities;
+
+namespace CoinCompassAPI.Application.Interface
+{
+    public interface IUserService
+    {
+        Task<bool> SignUp(SignUpDTO signUpDTO);
+        Task<SsoDTO> SignIn(SignInDTO signInDTO);
+        Task<ApplicationUser> GetCurrentUser();
+        Task<bool> DeleteUser(string userId);
+        void UpdateUser(ApplicationUser user);
+        Task<ApplicationUser> GetUserById(string userId);
+        Task<List<ApplicationUser>> ListUsers();
+    }
+}
